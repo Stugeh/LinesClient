057b1d6fd89e9ccaf3945403661bc0efb5e96ffa
stuge <tuukkaveteli@gmail.com>
2021-04-22 13:53:39 +0300

    join game button

--- src/components/MainMenu/GameList.js
+++ src/components/MainMenu/GameList.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 // import axios from 'axios';
 import {TextField, Table, TableContainer, TableHead,
-  TableBody, TableRow, TableCell, Paper} from '@material-ui/core';
+  TableBody, TableRow, TableCell, Paper, Button} from '@material-ui/core';
 
 import {useField} from '../../hooks/formHook';
 
@@ -29,26 +29,42 @@ const GameList = () => {
 
   const filteredGames = filterByHost(games, search.value);
 
-
-  console.log('search.value :>> ', search.value);
   return (
     <TableContainer component={Paper}>
       <Table aria-label="simple table">
 
         <TableHead>
           <TableRow>
-            <TableCell><TextField {...search} /></TableCell>
-            <TableCell>rules</TableCell>
+            <TableCell>
+              <TextField {...search} />
+              <h2 style={{marginBottom: '0px'}}>Player1</h2>
+            </TableCell>
+            <TableCell>
+              <h2 style={{marginBottom: '-30px'}}>rules</h2>
+            </TableCell>
           </TableRow>
         </TableHead>
 
         <TableBody>
           {filteredGames.map((game) => (
             <TableRow key={`${game.player1},${game.player2}`}>
-              <TableCell>{game.player1}</TableCell>
+              <TableCell>
+
+                <Button
+                  variant='contained'
+                  color='primary'
+                  style={{marginRight: '5px'}}
+                >
+                  join
+                </Button>
+
+                {game.player1}
+              </TableCell>
+
               <TableCell>
                 {`${game.rules.columns}x${game.rules.rows}`}
               </TableCell>
+
             </TableRow>
           ))}
         </TableBody>
