e5a8b558d0b134d45aca283bf0c3b72d5ed98643
stuge <tuukkaveteli@gmail.com>
2021-04-24 14:43:24 +0300

    frame for CreateGame

---
+++ src/components/CreateGame.js
@@ -0,0 +1,14 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+const CreateGame = ({setView}) => {
+  useEffect(()=>{
+
+  }, []);
+  return (
+    <div className='createGame'>
+
+    </div>
+  );
+};
+
+export default CreateGame;
