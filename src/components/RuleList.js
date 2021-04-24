90bc02d516205e84a0bcc31808cfcf367653f3c9
stuge <tuukkaveteli@gmail.com>
2021-04-24 18:33:12 +0300

    changes to rule list

--- src/components/RuleList.js
+++ src/components/RuleList.js
@@ -1,8 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import {
-  Table, TableContainer, TableHead,
-  TableBody, TableRow, TableCell, Paper,
+  Table, TableContainer, TableHead, TableBody, TableRow,
+  TableCell, TextField, Paper,
 } from '@material-ui/core';
 
 import {useField} from '../hooks/formHook';
@@ -19,71 +19,80 @@ const RuleList = ({}) => {
   // eslint-disable-next-line no-unused-vars
   const {reset: searchReset, ...search} = useField('text', 'search');
   const [ruleList, setRuleList] = useState([]);
+  // const [selectedRule, setSelectedRule] =useState('');
 
   useEffect(async ()=>{
-    const rules = await axios.get(`${API_URL}rules`, HEADERS);
+    const rules = await axios.get(`${API_URL}rules/`, HEADERS);
     setRuleList(rules.data.items);
   }, []);
 
   const filteredRules = filterByName(ruleList, search.value);
 
+  //   const handleRadio = (event) => {
+  //     setSelectedRule(event.target.value);
+  //   };
+
+  console.log(`selectedRule`, ruleList);
   return (
-    <TableContainer
-      style={{
-        width: '40%',
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        minWidth: '400px',
-      }}
-      component={Paper}
-    >
-      <Table aria-label="simple table">
+    <div style={{padding: '20px', margin: '0px'}}>
+      <TextField {...search} />
+      <TableContainer
+        style={{
+          width: '40%',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          minWidth: '400px',
+        }}
+        component={Paper}
+      >
+        <Table aria-label="simple table">
 
-        <TableHead>
-          <TableRow>
-            <TableCell>
-              <h3>Name</h3>
-            </TableCell>
-            <TableCell>
-              <h3 >Rows</h3>
-            </TableCell>
-            <TableCell>
-              <h3>Columns</h3>
-            </TableCell>
-            <TableCell>
-              <h3>Winning ticks</h3>
-            </TableCell>
-          </TableRow>
-        </TableHead>
-
-        <TableBody>
-          {filteredRules.map(((rule) => (
-            <TableRow key={`${rule['@controls'].self.href}`}>
+          <TableHead>
+            <TableRow >
               <TableCell>
-                {rule.name}
+                <h3>Name</h3>
               </TableCell>
               <TableCell>
-                {`${rule.rows}`}
+                <h3 >Rows</h3>
               </TableCell>
               <TableCell>
-                {`${rule.columns}`}
+                <h3>Columns</h3>
               </TableCell>
               <TableCell>
-                {`${rule.winning_tick_count}`}
+                <h3>Winning ticks</h3>
               </TableCell>
             </TableRow>
-          )))}
-        </TableBody>
+          </TableHead>
 
-      </Table>
-    </TableContainer>
+          <TableBody>
+            {filteredRules.map(((rule) => (
+              <TableRow hover key={`${rule['@controls'].self.href}`}>
+                <TableCell>
+                  {rule.name}
+                </TableCell>
+                <TableCell>
+                  {`${rule.rows}`}
+                </TableCell>
+                <TableCell>
+                  {`${rule.columns}`}
+                </TableCell>
+                <TableCell>
+                  {`${rule.winning_tick_count}`}
+                </TableCell>
+              </TableRow>
+            )))}
+          </TableBody>
+
+        </Table>
+      </TableContainer>
+    </div>
   );
 };
 
 const filterByName = (games, search) => {
   const filtered = search === '' ? games :
-  games.filter((rule) =>
-    rule.name.toLowerCase().includes(search.toLowerCase()));
+    games.filter((rule) =>
+      rule.name.toLowerCase().includes(search.toLowerCase()));
   return filtered;
 };
 
