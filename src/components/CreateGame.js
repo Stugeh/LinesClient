96404c0e630e8f5dafc1577f5636be53185d41a7
stuge <tuukkaveteli@gmail.com>
2021-04-25 13:35:47 +0300

    users can create games

--- src/components/CreateGame.js
+++ src/components/CreateGame.js
@@ -1,21 +1,38 @@
 import React, {useState} from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 import {Button} from '@material-ui/core';
 import RuleList from './RuleList';
-// const API_URL = process.env.REACT_APP_API_URL;
-// const HEADERS = {
-//   headers: {
-//     'Authorization': window.localStorage.getItem('authToken'),
-//   },
-// };
+const API_URL = process.env.REACT_APP_API_URL;
+const HEADERS = {
+  headers: {
+    'Authorization': window.localStorage.getItem('authToken'),
+  },
+};
 
 const CreateGame = ({setView}) => {
   const [selectedRule, setSelectedRule] = useState('');
+
+  const makeGame = async () => {
+    const res = await axios.post(
+        `${API_URL}games/`,
+        {rule: selectedRule},
+        HEADERS,
+    );
+    console.log('res :>> ', res);
+  };
+
   return (
     <div className='CreateRule'>
       <h2>Select a rule set</h2>
       <RuleList setSelectedRule={setSelectedRule}/>
       <h2>{selectedRule} selected</h2>
+      <Button
+        variant='contained'
+        style={{...playButtonStyle, backgroundColor: 'green'}}
+        onClick={makeGame}
+      >
+        Create Game
+      </Button><br/>
       <Button
         variant='contained'
         color='secondary'
@@ -26,6 +43,13 @@ const CreateGame = ({setView}) => {
       </Button>
     </div>
   );
+};
+
+const playButtonStyle = {
+  paddingLeft: '50px',
+  paddingRight: '50px',
+  fontSize: '30px',
+  marginTop: '50px',
 };
 
 export default CreateGame;
